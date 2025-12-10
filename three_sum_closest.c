02b31cc55faa3820b7dbc96d21640c2cedfac3bb
adityaverma9777 <supercellatcoc@gmail.com>
2025-12-10 22:51:27 +0530

    10/12/25 Question 16

---
+++ three_sum_closest.c
@@ -0,0 +1,36 @@
+int compare(const void* a, const void* b) {
+    return (*(int*)a - *(int*)b);
+}
+
+int abs_val(int x) {
+    return x < 0 ? -x : x;
+}
+
+int threeSumClosest(int* nums, int numsSize, int target) {
+    qsort(nums, numsSize, sizeof(int), compare);
+    
+    int closestSum = nums[0] + nums[1] + nums[2];
+    
+    for (int i = 0; i < numsSize - 2; i++) {
+        int left = i + 1;
+        int right = numsSize - 1;
+        
+        while (left < right) {
+            int sum = nums[i] + nums[left] + nums[right];
+            
+            if (abs_val(sum - target) < abs_val(closestSum - target)) {
+                closestSum = sum;
+            }
+            
+            if (sum < target) {
+                left++;
+            } else if (sum > target) {
+                right--;
+            } else {
+                return sum;
+            }
+        }
+    }
+    
+    return closestSum;
+}
