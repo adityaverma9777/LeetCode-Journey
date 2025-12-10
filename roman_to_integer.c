f1b08db17a34da446f6544de7009b3efeaf12c55
adityaverma9777 <supercellatcoc@gmail.com>
2025-12-10 22:51:18 +0530

    10/12/25 Question 13

---
+++ roman_to_integer.c
@@ -0,0 +1,26 @@
+int romanToInt(char* s) {
+    int values[26] = {0};
+    values['I' - 'A'] = 1;
+    values['V' - 'A'] = 5;
+    values['X' - 'A'] = 10;
+    values['L' - 'A'] = 50;
+    values['C' - 'A'] = 100;
+    values['D' - 'A'] = 500;
+    values['M' - 'A'] = 1000;
+    
+    int result = 0;
+    int len = strlen(s);
+    
+    for (int i = 0; i < len; i++) {
+        int current = values[s[i] - 'A'];
+        int next = (i + 1 < len) ? values[s[i + 1] - 'A'] : 0;
+        
+        if (current < next) {
+            result -= current;
+        } else {
+            result += current;
+        }
+    }
+    
+    return result;
+}
