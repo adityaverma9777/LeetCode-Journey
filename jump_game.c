cc4de5537b4b77d24ff92669218227c0505d459a
adityaverma9777 <supercellatcoc@gmail.com>
2025-12-11 16:15:01 +0530

    12/12/25 Question 55

---
+++ jump_game.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool canJump(int* nums, int numsSize) {
+    int maxReach = 0;
+    
+    for (int i = 0; i < numsSize; i++) {
+        if (i > maxReach) return false;
+        int reach = i + nums[i];
+        if (reach > maxReach) maxReach = reach;
+        if (maxReach >= numsSize - 1) return true;
+    }
+    
+    return true;
+}
