8fc77afb6d1237496dd1ffb09c8ba33729442441
adityaverma9777 <supercellatcoc@gmail.com>
2025-12-11 16:15:10 +0530

    12/12/25 Question 59

---
+++ spiral_matrix_ii.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int** generateMatrix(int n, int* returnSize, int** returnColumnSizes) {
+    int** matrix = (int**)malloc(n * sizeof(int*));
+    *returnColumnSizes = (int*)malloc(n * sizeof(int));
+    *returnSize = n;
+    
+    for (int i = 0; i < n; i++) {
+        matrix[i] = (int*)malloc(n * sizeof(int));
+        (*returnColumnSizes)[i] = n;
+    }
+    
+    int top = 0, bottom = n - 1, left = 0, right = n - 1;
+    int num = 1;
+    
+    while (top <= bottom && left <= right) {
+        for (int i = left; i <= right; i++)
+            matrix[top][i] = num++;
+        top++;
+        
+        for (int i = top; i <= bottom; i++)
+            matrix[i][right] = num++;
+        right--;
+        
+        for (int i = right; i >= left; i--)
+            matrix[bottom][i] = num++;
+        bottom--;
+        
+        for (int i = bottom; i >= top; i--)
+            matrix[i][left] = num++;
+        left++;
+    }
+    
+    return matrix;
+}
